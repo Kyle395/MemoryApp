c8d6cd451e531f8303bf3767a7e7989541c3bdb1
Kyle395 <wojciech.lukasz99@gmail.com>
2021-05-25 01:43:08 +0200

    Connects to server on ip 127.0.0.1 and port 1111

---
+++ MemoryClient/LoginScreen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryClient
+{
+    public partial class LoginScreen : Form
+    {
+        TcpClient Client = new TcpClient();
+        public LoginScreen()
+        {
+            Client = new TcpClient();
+
+            try
+            {
+                Client.Connect("127.0.0.1", 1111);
+
+                if (Client.Connected)
+                {
+                    InitializeComponent();
+                }
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Couldn't connect to the server. Please try again.");
+            }
+        }
+
+        private void LoginScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegistryScreen screen = new RegistryScreen(Client);
+            screen.ShowDialog();
+        }
+    }
+}
